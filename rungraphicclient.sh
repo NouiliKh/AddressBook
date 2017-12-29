230b23a10bcdf9911f06fe95fd9fae22fc0d5d6e
NouiliKh <khalil.nouili@hotmail.com>
2017-12-30 00:22:16 +0100

    fix graphicClient script

--- rungraphicclient.sh
+++ rungraphicclient.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd client
+cd ClientGraphicInterface
 rm *.class
 cp ../server/TreatmentInterface.class .
 cp ../server/TreatmentImpl_Stub.class .
@@ -7,4 +7,4 @@ cp ../server/FabTreatmentInterface.class .
 cp ../server/FabTreatmentImpl_Stub.class .
 cp ../server/Employee.class .
 javac -classpath lib/jfoenix-9.0.0.jar:../lib/javax.json.jar:./   Main.java
-javac -classpath lib/jfoenix-9.0.0.jar:../lib/javax.json.jar:./   Main
+java -classpath lib/jfoenix-9.0.0.jar:../lib/javax.json.jar:./   Main
